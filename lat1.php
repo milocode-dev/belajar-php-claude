3f4478753d42ad40669054aaf2940e8c6f77709c
milocode-dev <radityaakmal277@gmail.com>
2026-06-23 20:14:26 +0700

    Program 1, pengenalan variable dan penggunaan if else untuk mengecek apakah angka tersebut termasuk ganjil/genap

--- lat1.php
+++ lat1.php
@@ -1,3 +1,17 @@
 <?php
+    # Latihan Minggu 1 Dengan Panduan Claude
 
+    /* Di latihan pertama ini akan mengulik kembali tentang dasar pemrograman yaitu variable, tipe data (string, integer, float, bool, array),
+    operator, if/else, switch 
+    */
+
+    # Program 1 : Program Cek Bilangan Ganjil/Genap
+
+    $angka = 10;
+
+    if ($angka % 2 === 0) {
+        echo "Angka anda adalah $angka dan angka tersebut termasuk bilangan genap";
+    } else {
+        echo "Angka anda adalah $angka dan angka tersebut termasuk bilangan ganjil";    
+    }
 ?>
