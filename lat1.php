11435e271a557ca7f9e03275e4be10d3a146a515
milocode-dev <radityaakmal277@gmail.com>
2026-06-23 20:32:01 +0700

    Program 2, membuat kalkulator sederhana masih dengan if else tetapi juga terdapat nested if atau if bersarang atau if di dalam if

--- lat1.php
+++ lat1.php
@@ -11,7 +11,47 @@
 
     if ($angka % 2 === 0) {
         echo "Angka anda adalah $angka dan angka tersebut termasuk bilangan genap";
+
+        echo "<br>";
     } else {
         echo "Angka anda adalah $angka dan angka tersebut termasuk bilangan ganjil";    
+
+        echo "<br>";
+    }
+
+    # Program 2 : Program Kalkulator Sederhana
+
+    // Langkah-langkah (Agar terbiasa menulis apa yang perlu dilakukan terlebih dahulu)
+    # Harus mempunyai 2 bilangan untuk diprogram
+    # Menentukan kedua bilangan tersebut akan dioperasikan dengan apa + - * /
+    # Logika untuk mengoperasikan
+    # Echo hasil
+
+    $angka1 = 10;
+    $angka2 = 0;
+    $operator = "/";
+
+    if ($operator == "+" ) {
+        $hasil = $angka1 + $angka2;
+
+        echo "Hasil dari $angka1 + $angka2 adalah $hasil <br>";
+    } elseif ($operator == "-") {
+        $hasil = $angka1 - $angka2;
+
+        echo "Hasil dari $angka1 - $angka2 adalah $hasil <br>";
+    } elseif ($operator == "*") {
+        $hasil = $angka1 * $angka2;
+
+        echo "Hasil dari $angka1 * $angka2 adalah $hasil <br>";
+    } elseif ($operator == "/") {
+        if ($angka2 === 0) {
+            echo "Angka tidak logis!";
+        } else {
+        $hasil = $angka1 / $angka2;
+
+        echo "Hasil dari $angka1 / $angka2 adalah $hasil <br>";
+        }
+    } else {
+        echo "Operator tidak tersedia <br>";
     }
 ?>
