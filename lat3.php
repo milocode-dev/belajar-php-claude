1734982c6c656cd1930c78d1b9d9cee6258bada5
milocode-dev <radityaakmal277@gmail.com>
2026-06-24 13:39:21 +0700

    Membuat class dengan bantuan __construct agar penulisan kode lebih singkat dan lebih rapi

--- lat3.php
+++ lat3.php
@@ -7,12 +7,19 @@
     class Mobil {
         public $merk;
         public $warna;
+        public $type;
         public $kecepatan = 0;
+
+        function __construct($merk, $type, $warna) {
+            $this->merk = $merk;
+            $this->type = $type;
+            $this->warna = $warna;
+        }
 
         function jalan() {
             $this->kecepatan = 60;
 
-            echo "Mobil $this->merk berwarna $this->warna berkendara dengan kecepatan $this->kecepatan <br>";
+            echo "Mobil $this->merk $this->type  berwarna $this->warna berkendara dengan kecepatan $this->kecepatan km/h <br>";
         }
 
         function rem() {
@@ -21,9 +28,7 @@
         }
     }
 
-    $newMobil = new Mobil();
-    $newMobil->merk = "Avanza";
-    $newMobil->warna = "Putih";
+    $newMobil = new Mobil("BMW", "M4", "Black");
     $newMobil->jalan();
     $newMobil->rem();
 
