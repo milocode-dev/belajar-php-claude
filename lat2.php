ce4f8ab37d3053ef46fec4e6acf63238c4adcfe6
milocode-dev <radityaakmal277@gmail.com>
2026-06-24 13:21:34 +0700

    Membuat fungsi untuk menghitung rata rata

--- lat2.php
+++ lat2.php
@@ -21,12 +21,24 @@
     echo hitungLuas("10", "20");
     echo "<br>";
 
+    # Program 3 : Membuat Array dan menampilkan hanya nama yang memiliki jumlah string lebih dari 5
 
     $names = ["Pask ", "Jids ", "Prabss ", "Jisuds ", "Ojadn "];
 
     foreach ($names as $name) {
         if (strlen($name) > 5) {
             echo $name;
+            echo "<br>";
         }
     }
+
+    # Tes Mandiri : Membuat function yang menerima array angka dan return rata-ratanya
+
+    function hitungRataRata($angka) {
+        return array_sum($angka) / count($angka);
+    }
+
+    $data = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"];
+
+    echo hitungRataRata($data);
 ?>
