6c25578c8c8d9b85eb221cb5f52652ba79392a42
milocode-dev <radityaakmal277@gmail.com>
2026-06-25 15:34:07 +0700

    Bagian 2

--- tes.php
+++ tes.php
@@ -20,4 +20,20 @@
 
     echo "Anda mendapatkan diskon sebesar", " " , hitungDiskon("200000", "silver"), " ", "rupiah <br>";
 
+    # Bagian 2 : Loop & Array
+    # Membuat array 5 produk dan menampilkan produk yang harganya di atas 50000
+
+    $produk = [
+        ["nama" => "iPhone 13", "harga" => 80000],
+        ["nama" => "Gitar", "harga" => 25000],
+        ["nama" => "Kipas", "harga" => 35000],
+        ["nama" => "Laptop", "harga" => 60000],
+        ["nama" => "Parfum", "harga" => 45000],
+    ];
+
+    foreach ($produk as $item) {
+        if($item["harga"] > 50000) {
+            echo $item["nama"] . " - Rp" . $item["harga"] . "<br>";
+        }
+    }
 ?>
